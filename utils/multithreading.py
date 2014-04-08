076a72435a6408907836ec64fb07bb1d2b18dbd9
Kalimaha <guido.barbaglia@fao.org>
2014-04-08 11:45:14 +0200

    Parametric outputs

--- utils/multithreading.py
+++ utils/multithreading.py
@@ -13,6 +13,9 @@ except Exception, e:
 exitFlag = 0
 c = config.Config('MODIS')
 l = log.Logger()
+product = 'MOD13A2'
+year = '2014'
+day = '001'
 
 class MODIS(threading.Thread):
 
@@ -32,14 +35,14 @@ def process_data(threadName, q):
         queueLock.acquire()
         if not workQueue.empty():
             data = q.get()
-            raster.modisDownloadExtractDelete('MOD13A2', '2014', '001', data)
+            raster.modisDownloadExtractDelete(product, year, '001', data)
             queueLock.release()
             l.info(threadName + ' processing ' + data)
         else:
             queueLock.release()
         time.sleep(1)
 
-layers = ftp.listDir('ladsweb.nascom.nasa.gov', '/allData/5/MOD13A2/2014/001/')
+layers = ftp.listDir('ladsweb.nascom.nasa.gov', '/allData/5/' + product + '/' + year + '/' + day + '/')
 
 threadList = []
 for i in range(0, c.get('threads')):
@@ -76,18 +79,43 @@ for t in threads:
     t.join()
 l.info('Exiting Main Thread')
 
-l.info('Create global hdf: START...')
-if not os.path.exists('/home/kalimaha/Desktop/MODIS/MOD13A2/2014/001/EVI/output/out.hdf'):
-    os.system('gdal_merge.py -n -3000 -a_nodata 0 /home/kalimaha/Desktop/MODIS/MOD13A2/2014/001/EVI/tmp/*.hdf -o /home/kalimaha/Desktop/MODIS/MOD13A2/2014/001/EVI/output/out.hdf')
-l.info('Create global hdf: DONE.')
+# Read configuration file
+bands = c.get('bands')
+subfolders = c.get('subfolders')
 
-l.info('Create 4326 TIF: START...')
-if not os.path.exists('/home/kalimaha/Desktop/MODIS/MOD13A2/2014/001/EVI/output/out_4326.hdf'):
-    os.system("gdalwarp -srcnodata 0 -dstnodata nodata -multi -of GTiff -tr 0.00833333 -0.00833333  -s_srs '+proj=sinu +R=6371007.181 +nadgrids=@null +wktext' -co 'TILED=YES' -t_srs EPSG:4326 /home/kalimaha/Desktop/MODIS/MOD13A2/2014/001/EVI/output/out.hdf /home/kalimaha/Desktop/MODIS/MOD13A2/2014/001/EVI/output/out_4326.tif")
-l.info('Create 4326 TIF: DONE.')
+# Iterate over bands
+for band in bands:
 
-l.info('Create overviews: START...')
-if not os.path.exists('/home/kalimaha/Desktop/MODIS/MOD13A2/2014/001/EVI/output/out_4326_overviews.hdf'):
-    os.system('cp /home/kalimaha/Desktop/MODIS/MOD13A2/2014/001/EVI/output/out_4326.tif /home/kalimaha/Desktop/MODIS/MOD13A2/2014/001/EVI/output/out_4326_overviews.tif')
-    os.system('gdaladdo -r average /home/kalimaha/Desktop/MODIS/MOD13A2/2014/001/EVI/output/out_4326_overviews.tif 2 4 8 16')
-l.info('Create overviews: DONE.')
+    # Create global HDF
+    l.info('Create global HDF for ' + band + ': START...')
+    name = product + '.' + year + '.' + day + '.' + band + '.hdf'
+    src = c.get('targetDir') + '/' + product + '/' + year + '/' + day + '/' + band + '/' + subfolders['tmp'] + '/*.hdf'
+    out = c.get('targetDir') + '/' + product + '/' + year + '/' + day + '/' + band + '/' + subfolders['output'] + '/' + name
+    if not os.path.exists(out):
+        os.system('gdal_merge.py -n -3000 -a_nodata 0 ' + src + ' -o ' + out)
+    else:
+        l.info(out + ' already exists')
+    l.info('Create global HDF for ' + band + ': DONE.')
+
+    # Create 4326 projection
+    l.info('Create 4326 TIF for ' + band + ': START...')
+    name = product + '.' + year + '.' + day + '.' + band + '.4326.tif'
+    src = c.get('targetDir') + '/' + product + '/' + year + '/' + day + '/' + band + '/' + subfolders['output'] + '/' + name.replace('.4326.hdf', '.hdf')
+    out = c.get('targetDir') + '/' + product + '/' + year + '/' + day + '/' + band + '/' + subfolders['output'] + '/' + name
+    if not os.path.exists(out):
+        os.system("gdalwarp -srcnodata 0 -dstnodata nodata -multi -of GTiff -tr 0.00833333 -0.00833333  -s_srs '+proj=sinu +R=6371007.181 +nadgrids=@null +wktext' -co 'TILED=YES' -t_srs EPSG:4326 " + src + " " + out)
+    else:
+        l.info(out + ' already exists')
+    l.info('Create 4326 TIF for ' + band + ': DONE.')
+
+    # Create overviews
+    l.info('Create overviews for ' + band + ': START...')
+    cp = c.get('targetDir') + '/' + product + '/' + year + '/' + day + '/' + band + '/' + subfolders['output'] + '/' + name
+    name = product + '.' + year + '.' + day + '.' + band + '.4326.OVERVIEWS.tif'
+    out = c.get('targetDir') + '/' + product + '/' + year + '/' + day + '/' + band + '/' + subfolders['output'] + '/' + name
+    if not os.path.exists(out):
+        os.system('cp ' + cp + ' ' + out)
+        os.system('gdaladdo -r average ' + out + ' 2 4 8 16')
+    else:
+        l.info(out + ' already exists')
+    l.info('Create overviews for ' + band + ': DONE.')

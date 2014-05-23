04f04d597208f49c65c259028bb101f2ab6cf5ac
Vortex- <simone.murzilli@fao.org>
2014-05-23 10:31:11 +0200

    geometadata

---
+++ tests/geometadata.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestGeo(unittest.TestCase):
+
+    def test_import(self):
+        print self.seq
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
